87af028cc099e7d3b38199e30df0b9c43b9d7231
mmilito <militomariel@gmail.com>
2015-01-19 16:09:17 -0700

    exercise complete

---
+++ main.js
@@ -0,0 +1,54 @@
+/* LOOP 1 = 0 100 200 300 400 500 
+
+for (var i=0; i<6; i++) {
+	console.log(i*100);
+	// document.write(i*100);
+}
+*/
+
+
+/* LOOP 2 = 2 4 8 16 32 64 
+
+for (var i=2; i<=64; i*=2) {
+	console.log(i);
+}
+*/
+
+/* LOOP 3 = 1 1 1 2 2 2 3 3 3
+
+for (var i=1; i<=3; i++) {
+	for (var j=1; j<=3; j++) {
+			document.write(i);
+		// console.log(i);
+	}
+} */
+
+/*
+ LOOP 4 = 0 2 4 6 8 10
+
+for (var i=0; i<=10; i+=2 ) {
+	document.write(i);
+	// console.log(i);
+} */
+
+/* LOOP 5 = 3 6 9 12 15 
+
+for (var i=3; i<=15; i+=3) {
+	 console.log(i);
+}
+*/
+
+/* LOOP 6 = 9 8 7 6 5 4 3 2 1 0 
+
+for (var i=9; i>=0; i--) {
+	console.log(i);
+}
+*/
+
+ // LOOP 7 = 0 1 2 3 0 1 2 3 0 1 2 3
+for (var i=1; i<=3; i++) {
+	for (var j=0; j<=3; j++) {
+			document.write(j);
+		// console.log(i);
+	}
+}
